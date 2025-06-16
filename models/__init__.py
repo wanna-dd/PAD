ad2d3f0aee09e222d297bdc6114e9556483db734
Wanna <64192307+wanna-dd@users.noreply.github.com>
2025-06-16 17:09:15 +0800

    __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,12 +5,7 @@ from .MobileNetV3 import *
 
 
 
-# def load_model(name, num_classes=4, pretrained=False, **kwargs):
-#
-#     model_dict = globals()
-#     model = model_dict[name](pretrained=pretrained, num_classes=num_classes, **kwargs)
-#     return model
-def load_model(name, num_classes=5, **kwargs):
+def load_model(name, num_classes=X, **kwargs):
 
     model_dict = globals()
     model = model_dict[name](num_classes=num_classes, **kwargs)
