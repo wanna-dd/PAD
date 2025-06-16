eead8f60fccbc64a306d06d3b3fca0464db458da
Wanna <64192307+wanna-dd@users.noreply.github.com>
2025-06-16 16:55:59 +0800

    Add files via upload

---
+++ datasets.py
@@ -0,0 +1,244 @@
+import bisect
+import warnings
+import numpy as np, random, os
+import pandas as pd
+import torch
+from scipy.stats import sampling
+from torch.utils.data import Sampler, Dataset, DataLoader, BatchSampler, SequentialSampler, RandomSampler, TensorDataset
+from collections import defaultdict
+
+
+def setup_seed(seed, deterministic=False):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)  # CPU
+    torch.cuda.manual_seed(seed)  # GPU
+    torch.cuda.manual_seed_all(seed)  # All GPU
+    os.environ['PYTHONHASHSEED'] = str(seed)  # 禁止hash随机化
+    if deterministic:  # 以下操作涉及cudnn，一般为False，也可以设置为True，尝试一下。
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+    torch.use_deterministic_algorithms(True)
+
+
+class PairBatchSampler(Sampler):
+    def __init__(self, dataset, batch_size, num_iterations=None):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.num_iterations = num_iterations
+
+    def __iter__(self):
+        indices = list(range(len(self.dataset)))
+        random.shuffle(indices)
+        for k in range(len(self)):
+            if self.num_iterations is None:
+                offset = k*self.batch_size
+                batch_indices = indices[offset:offset+self.batch_size]
+            else:
+                # 从数据集中随机抽取batch_size个样本索引
+                batch_indices = random.sample(range(len(self.dataset)),
+                                              self.batch_size)
+
+            pair_indices = []
+            for idx in batch_indices:
+                # 获取样本的类别
+                y = self.dataset.get_class(idx).item()
+                if not self.dataset.classwise_indices[y]:
+                    raise ValueError("No samples available for class {y}")
+                # 从同类样本中随机选择一个样本
+                pair_indices.append(random.choice(self.dataset.classwise_indices[y]))
+
+            yield batch_indices + pair_indices
+
+    def __len__(self):
+        if self.num_iterations is None:
+            return (len(self.dataset)+self.batch_size-1) // self.batch_size
+        else:
+            return self.num_iterations
+
+
+class DatasetWrapper(Dataset):
+
+    def __init__(self, dataset, indices=None):
+        self.base_dataset = dataset
+        if indices is None:
+            self.indices = list(range(len(dataset)))
+        else:
+            self.indices = indices
+
+        # 创建一个defaultdict，用于存储每个类的样本索引列表
+        self.classwise_indices = defaultdict(list)
+        for i in range(len(self)):
+            y = self.base_dataset[self.indices[i]][1]  # 获取目标值
+            self.classwise_indices[y.item()].append(i)  # .item() 将单元素张量转换为 Python 标量
+        self.num_classes = max(self.classwise_indices.keys()) + 1
+        # print("Classwise Indices:", dict(self.classwise_indices))
+
+        # 重写__getitem__方法
+    def __getitem__(self, i):
+        return self.base_dataset[self.indices[i]]
+
+    def __len__(self):
+        return len(self.indices)
+
+    def get_class(self, i):
+        return self.base_dataset[self.indices[i]][1]  # 返回目标值
+
+
+class ConcatWrapper(Dataset): # TODO: Naming
+    @staticmethod
+    def cumsum(sequence):
+        r, s = [], 0
+        for e in sequence:
+            l = len(e)
+            r.append(l + s)
+            s += l
+        return r
+
+    @staticmethod
+    def numcls(sequence):
+        s = 0
+        for e in sequence:
+            l = e.num_classes
+            s += l
+        return s
+
+    @staticmethod
+    def clsidx(sequence):
+        r, s, n = defaultdict(list), 0, 0
+        for e in sequence:
+            l = e.classwise_indices
+            for c in range(s, s + e.num_classes):
+                t = np.asarray(l[c-s]) + n
+                r[c] = t.tolist()
+            s += e.num_classes
+            n += len(e)
+        return r
+
+    def __init__(self, datasets):
+        super(ConcatWrapper, self).__init__()
+        assert len(datasets) > 0, 'datasets should not be an empty iterable'
+        self.datasets = list(datasets)
+        # for d in self.datasets:
+        #     assert not isinstance(d, IterableDataset), "ConcatDataset does not support IterableDataset"
+        self.cumulative_sizes = self.cumsum(self.datasets)
+
+        self.num_classes = self.numcls(self.datasets)
+        self.classwise_indices = self.clsidx(self.datasets)
+
+    def __len__(self):
+        return self.cumulative_sizes[-1]
+
+    def __getitem__(self, idx):
+        if idx < 0:
+            if -idx > len(self):
+                raise ValueError("absolute value of index should not exceed dataset length")
+            idx = len(self) + idx
+        dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
+        if dataset_idx == 0:
+            sample_idx = idx
+        else:
+            sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
+        return self.datasets[dataset_idx][sample_idx]
+
+    def get_class(self, idx):
+        if idx < 0:
+            if -idx > len(self):
+                raise ValueError("absolute value of index should not exceed dataset length")
+            idx = len(self) + idx
+        dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
+        if dataset_idx == 0:
+            sample_idx = idx
+        else:
+            sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
+        return self.datasets[dataset_idx].get_class(sample_idx)
+
+    @property
+    def cummulative_sizes(self):
+        warnings.warn("cummulative_sizes attribute is renamed to "
+                      "cumulative_sizes", DeprecationWarning, stacklevel=2)
+        return self.cumulative_sizes
+
+def input_tensor(data, label):
+    data = np.array(data)
+    data = torch.FloatTensor(data)
+
+    label = np.array(label)
+    label = label.astype(float)
+    label = torch.LongTensor(label)
+
+    return data, label
+
+def read_excel(path):
+    read_excel = pd.read_excel(path, engine='openpyxl')
+    np_list = read_excel.T.to_numpy()
+    data = np_list[0:-1].T
+    data = np.float64(data)
+    label = np_list[-1]
+
+    data, label = input_tensor(data, label)
+
+    return data, label
+
+def load_dataset(root, sample='default', **kwargs):
+    setup_seed(42)
+    # Dataset
+    # train_val_dataset_dir = os.path.join(root, "train_320_25.10.xlsx")
+    # test_dataset_dir = os.path.join(root, "val_80_25.10.xlsx")
+    train_val_dataset_dir = os.path.join(root, "train_320_0616_比较预处理.xlsx")
+    test_dataset_dir = os.path.join(root, "val_80_0616_比较预处理.xlsx")
+
+    trainset_data, trainset_targets = read_excel(train_val_dataset_dir)
+    valset_data, valset_targets = read_excel(test_dataset_dir)
+
+    trainset_dataset = TensorDataset(trainset_data, trainset_targets)
+    valset_dataset = TensorDataset(valset_data, valset_targets)
+
+    trainset = DatasetWrapper(trainset_dataset)
+    valset = DatasetWrapper(valset_dataset)
+
+    # Sampler
+    if sample == 'default':
+        get_train_sampler = lambda d: BatchSampler(RandomSampler(d), kwargs['batch_size'], False)
+        get_test_sampler  = lambda d: BatchSampler(SequentialSampler(d), kwargs['batch_size'], False)
+
+    elif sample == 'pair':
+        get_train_sampler = lambda d: PairBatchSampler(d, kwargs['batch_size'])
+        get_test_sampler  = lambda d: BatchSampler(SequentialSampler(d), kwargs['batch_size'], False)
+
+    else:
+        raise Exception('Unknown sampling: {}'.format(sampling))
+
+    trainloader = DataLoader(trainset, batch_sampler=get_train_sampler(trainset), num_workers=0)
+    valloader   = DataLoader(valset,   batch_sampler=get_test_sampler(valset), num_workers=0)
+
+    return trainloader, valloader
+
+
+def load_test_dataset(root, row_index=None, **kwargs):
+    setup_seed(42)
+    # Dataset
+    # test_dataset_dir = os.path.join(root, "test_100_25.10.xlsx")  # 25.10.24
+    test_dataset_dir = os.path.join(root, "test_100_0616_比较预处理.xlsx")  # 06.16.25
+    testset_data, testset_targets = read_excel(test_dataset_dir)
+
+    # 如果指定了 num_rows，则只选择前 num_rows 行
+    if row_index is not None:
+        testset_data = testset_data[row_index:row_index+1]
+        testset_targets = testset_targets[row_index:row_index+1]
+
+
+    testset_dataset = TensorDataset(testset_data, testset_targets)
+    testset = DatasetWrapper(testset_dataset)
+
+    get_test_sampler  = lambda d: BatchSampler(SequentialSampler(d), kwargs['batch_size'], False)
+    testloader   = DataLoader(testset, batch_sampler=get_test_sampler(testset), num_workers=0)
+
+    return testloader
+
+
+
+
+
+
